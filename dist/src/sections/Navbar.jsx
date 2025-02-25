a09c399e619e75fac8654397fdb5951188034bc4
TaxiMax1 <taximaxi1808@gmail.com>
2025-02-25 15:45:46 -0300

    fixed links

--- dist/src/sections/Navbar.jsx
+++ dist/src/sections/Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = () => {
             <nav className="navbar-container">
                 <div className="nav-links">
                     <a href="/"><h1>Home</h1></a>
-                    <a href="/about"><h1>About</h1></a>
-                    <a href="/projects"><h1>Projects</h1></a>
+                    <a href="/#/about"><h1>About</h1></a>
+                    <a href="/#/projects"><h1>Projects</h1></a>
                     <a href="https://discord.gg/5Y8hqr64fJ"><h1>Contact</h1></a>
                 </div>
             </nav>
